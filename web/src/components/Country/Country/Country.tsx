712c4fd8d373fa1320e11324c75e2cda1f05c51b
anyushu <anyushu0526@gmail.com>
2022-07-26 12:40:14 +0900

    lint fix

--- web/src/components/Country/Country/Country.tsx
+++ web/src/components/Country/Country/Country.tsx
@@ -1,5 +1,3 @@
-import humanize from 'humanize-string'
-
 import { Link, routes, navigate } from '@redwoodjs/router'
 import { useMutation } from '@redwoodjs/web'
 import { toast } from '@redwoodjs/web/toast'
@@ -11,39 +9,6 @@ const DELETE_COUNTRY_MUTATION = gql`
     }
   }
 `
-
-const formatEnum = (values: string | string[] | null | undefined) => {
-  if (values) {
-    if (Array.isArray(values)) {
-      const humanizedValues = values.map((value) => humanize(value))
-      return humanizedValues.join(', ')
-    } else {
-      return humanize(values as string)
-    }
-  }
-}
-
-const jsonDisplay = (obj) => {
-  return (
-    <pre>
-      <code>{JSON.stringify(obj, null, 2)}</code>
-    </pre>
-  )
-}
-
-const timeTag = (datetime) => {
-  return (
-    datetime && (
-      <time dateTime={datetime} title={datetime}>
-        {new Date(datetime).toUTCString()}
-      </time>
-    )
-  )
-}
-
-const checkboxInputTag = (checked) => {
-  return <input type="checkbox" checked={checked} disabled />
-}
 
 const Country = ({ country }) => {
   const [deleteCountry] = useMutation(DELETE_COUNTRY_MUTATION, {
